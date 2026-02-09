2c2cde6d31f062059f33346f37fdeaa60470f620
maeeast <maeeast@me.com>
2026-02-09 21:33:44 +0000

    Initial commit of existing project files

---
+++ default.php
@@ -0,0 +1,46 @@
+The purpose of this site is to practice my own development skills.
+<br><Br>
+It will evolve from this blank slate to a fully-developed personal website.
+
+<!-- I plan to put a header which is a panoramic sunset photo from my home.
+The menu will be down the left.
+I plan to have a section for summary of me, a section to outline a bit about the menagerie I care for, my professional career (which will be an interactive resume thatis built on a database of my experience and a work sample archive), my writing (some samples), and some other hobbies 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+<title>Justin T. Cole - Profile</title>
+<meta http-equiv="description" content="This is a general information site for Justin T. Cole" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "styles.css";</style>
+</head>
+
+<body>
+
+<?php include("includes/header.html");?>
+<?php include("includes/navigation.html");?>
+
+This is the content of the page
+
+<?php include("includes/footer.html");?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-->
